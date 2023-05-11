34b499256a6c47b4aabbef4d2e94ab002c49ab90
Caren-Koroeny <carensiya5@gmail.com>
2023-05-11 17:37:13 +0300

    Created method for filter

--- myenumerable_file.rb
+++ myenumerable_file.rb
@@ -3,8 +3,14 @@ module Myenumerable
       each { |element| return false unless yield(element) }
       true
     end
-    
+
     def any?
         each { |element| return true if yield(element) }
         false
       end
+      def filter
+        result = []
+        each { |element| result << element if yield(element) }
+        result
+      end
+    end
