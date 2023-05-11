0f54898716d8138c2c6e29a3eebe22fb3fec2829
Caren-Koroeny <carensiya5@gmail.com>
2023-05-11 17:36:31 +0300

    Created method for any

--- myenumerable_file.rb
+++ myenumerable_file.rb
@@ -3,3 +3,8 @@ module Myenumerable
       each { |element| return false unless yield(element) }
       true
     end
+    
+    def any?
+        each { |element| return true if yield(element) }
+        false
+      end
